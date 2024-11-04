4f89c25d6e1337efb621b73ff596f19757166dd5
miftah1299 <jannat27120@gmail.com>
2024-11-05 02:33:16 +0600

    add Banner component, enhance Coffees and Dashboard pages with descriptions, update routing and Navbar links

--- src/pages/Coffees.jsx
+++ src/pages/Coffees.jsx
@@ -2,7 +2,10 @@
 const Coffees = () => {
     return (
         <div>
-            
+            <h2>
+                This is the Coffees page. Here you can view the list of coffees and see
+                the details of each coffee. Enjoy!
+            </h2>
         </div>
     );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,10 @@
 const Dashboard = () => {
     return (
         <div>
-            
+            <h2>
+                This is the Dashboard page. Here you can view the list of coffees and see
+                the details of each coffee. Enjoy!
+            </h2>
         </div>
     );
 };
