c0c886e3534260c0492925b4a6408eaa5c5195c6
Your Name <jannat27120@gmail.com>
2024-11-05 21:02:05 +0600

    Replace Link with NavLink for active styling

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Categories = ({ categories }) => {
     return (
         <div role="tablist" className="tabs tabs-bordered">
             {categories.map((category) => (
-                <Link
+                <NavLink
                     key={category.category}
                     to={`/category/${category.category}`}
                     role="tab"
-                    className="tab"
+                    className={({ isActive }) =>
+                        `tab ${isActive ? "tab-active" : ""}`
+                    }
                 >
                     {category.category}
-                </Link>
+                </NavLink>
             ))}
         </div>
     );
