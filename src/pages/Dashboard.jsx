cd9258d7857dfd8d12a2cb6bffc5a66de57da744
Your Name <jannat27120@gmail.com>
2024-11-06 04:21:19 +0600

    add Dashboard page to display favorite coffees; refactor Heading component for better structure

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react";
+import Heading from "../components/Heading";
+import { getAllFavorites } from "../utilities";
+import Card from "../components/Card";
 
 const Dashboard = () => {
+    const [coffees, setCoffees] = useState([]);
+    useEffect(() => {
+        const favorites = getAllFavorites();
+        setCoffees(favorites);
+    }, []);
+
     return (
         <div>
-            <h2>
-                This is the Dashboard page. Here you can view the list of coffees and see
-                the details of each coffee. Enjoy!
-            </h2>
+            <Heading
+                title="Welcome to Dashboard"
+                subtitle="Manage coffee that you love"
+            />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {coffees.map((coffee) => (
+                    <Card key={coffee.id} coffee={coffee}></Card>
+                ))}
+            </div>
         </div>
     );
 };
